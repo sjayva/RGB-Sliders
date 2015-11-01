908d24bda1b7680478f7597b7198e738205d4603
jayschic <sjayva@gmail.com>
2015-11-01 13:12:27 -0600

    Initial Commit

---
+++ RGBSliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGBSliders
+//
+//  Created by Jayaram, Sheethal on 11/1/15.
+//  Copyright © 2015 Jayaram, Sheethal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
